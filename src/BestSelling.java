5ab7e3eac2068b40ed70a3a386954a7933ff6c3c
Gia <gjackson24@scienceleadership.org>
2022-11-21 11:02:50 -0500

    Initial commit

---
+++ src/BestSelling.java
@@ -0,0 +1,39 @@
+public class BestSelling {
+     private String name;
+    private String year;
+    private int sales;
+
+    public BestSelling(String name, String year, int sales) {
+        this.name = name;
+        this.year = year;
+        this.sales = sales;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getYear() {
+        return year;
+    }
+
+    public void setYear(String year) {
+        this.year = year;
+    }
+
+    public int getSales() {
+        return sales;
+    }
+
+    public void setSales(int sales) {
+        this.sales = sales;
+    }
+
+    void describeData(){
+        System.out.println("The best selling item is " + name + " it was made in " + year + " and had " + sales + " sales. ");
+    }
+}
