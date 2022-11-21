1bde14f2e8744b14be308460ea7c3b11a5170e5f
dinajdadollani <ddollani24@scienceleadership.org>
2022-11-21 11:10:17 -0500

    .

---
+++ src/Candy.java
@@ -0,0 +1,10 @@
+public class Candy extends BestSelling{
+    private String brand;
+
+    private String country;
+
+
+    public Candy(String name, String year, int sales) {
+        super(name, year, sales);
+    }
+}
