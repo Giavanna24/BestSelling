d4b137ea092289ce11193bfae09566935d682416
Gia <gjackson24@scienceleadership.org>
2022-12-09 13:09:34 -0500

    Printed Directly

--- src/edu/sla/bestselling/Main.java
+++ src/edu/sla/bestselling/Main.java
@@ -1,13 +1,15 @@
 package edu.sla.bestselling;
 
+
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception{
 
         new Candy("Reeses", 1923, 2603, "Reeses Peanut Butter Cups", "United States");
         new Books("A Tale of Two Cities", 1859, 200000000, "Charles Dicknens", "English", "Historical Fiction");
 
         for (BestSelling best : BestSelling.getAllBestSelling())
             System.out.println(best);
+        Books.Read();
     }
 }
